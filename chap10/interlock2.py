9c470a603a0e4ce5b0455bfe62ce1dc9de0f476c
Mark Giolando <mark-robin.giolando@students.olin.edu>
2013-11-28 12:26:46 -0500

    extra

--- chap10/interlock2.py
+++ chap10/interlock2.py
@@ -1,17 +1,29 @@
+def interlock(word,word_list):
+	"""Finds two words that interlcok to make a third word.
+	
+	word: string
+	word_list: list
 
-
-def interlock(k,word_list):
-	word1=k[::2]
-	word2=k[1::2]
+	returns: tuple #I didn't realize it was a tuple at the original time of writing
+	"""
+	word1=word[::2]
+	word2=word[1::2]
 	if word1 in word_list and word2 in word_list:
-		print k,word1,word2
+		print word,word1,word2
 		return word1,word2
-def interlock3(k,word_list):
-	word1=k[::3]
-	word2=k[1::3]
-	word3=k[2::3]
+def interlock3(word,word_list):
+	"""Finds three words that interlock to make a fourth word.
+	
+	word: string
+	word_list: list
+
+	returns: tuple #I didn't realize it was a tuple at the original time of writing
+	"""
+	word1=word[::3]
+	word2=word[1::3]
+	word3=word[2::3]
 	if word1 in word_list and word2 in word_list and word3 in word_list:
-		print k,word1,word2,word3
+		print word,word1,word2,word3
 		return word1,word2,word3
 	
 
@@ -21,7 +33,7 @@ if __name__ == '__main__':
     for line in fin:
 	entry=line.strip()
 	word_list.append(entry)
-    for i in range(len(word_list)):
-	#interlock(word_list[i],word_list)
-        interlock3(word_list[i],word_list)
+    for word in word_list:
+	interlock(word,word_list)
+        #interlock3(word,word_list)
 
