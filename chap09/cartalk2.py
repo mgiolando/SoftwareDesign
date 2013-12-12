0b9e1e27ae1218fd3a56a8cb8da0c8959f817cda
Mark Giolando <mark-robin.giolando@students.olin.edu>
2013-12-11 19:01:13 -0500

    update variable names

--- chap09/cartalk2.py
+++ chap09/cartalk2.py
@@ -21,14 +21,16 @@ def is_swaped(a,b,test_diff):
 	
 	returns: int
 	"""
-	af=a/10
-	al=a%10
-	bf=b/10
-	bl=b%10
+	#a is the first number, in this case the mother's age
+	#b is the second number tested, in this case the daughter's age
+	af=a/10 #af is the first number of a
+	al=a%10 #al is the last number of a
+	bf=b/10 #bf is the first number of b
+	bl=b%10 #bl is the first number of b
 	count=0
 	if af==bl and bf==al and a>b and counting(a,b,test_diff)>0:
 		count+=counting(a,b,test_diff)
-		if abs(a-b)==18:#I found the age difference was 18. Then I came back to add this to print out the times the ages were reversed adn counted to reach the answer.
+		if abs(a-b)==18 and a>b:#I found the age difference was 18. Then I came back to add this to print out the times the ages were reversed adn counted to reach the answer.
 			print a,b
 		return 1
 	else:
@@ -40,11 +42,11 @@ def run_tests(test_diff):
 	test_diff: int
 	"""
 	count=0
-	for a in range (100):
-		for b in range(100):
+	for age_moth in range (100):
+		for age_daug in range(100):
 			
-			is_swaped(a,b,test_diff)
-			count+=is_swaped(a,b,test_diff)
+			is_swaped(age_moth,age_daug,test_diff)
+			count+=is_swaped(age_moth,age_daug,test_diff)
 	if count ==8:
 		print test_diff,'age diff'
 
