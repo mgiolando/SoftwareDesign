117668039827e770115ddb7560a0266cfee0ed00
Mark Giolando <mark-robin.giolando@students.olin.edu>
2013-12-12 22:01:24 -0500

    update

--- chap14/choose_sixth.py
+++ chap14/choose_sixth.py
@@ -1,10 +1,5 @@
 import urllib
-#conn = urllib.urlopen('http://thinkpython.com/secret.html')
-#for line in conn:
-#	print line.strip()
-#'http://www.uszip.com/zip/02492'
-
-if __name__ == '__main__':
+def run_script():
     var=raw_input("Enter a Zipcode: ")
     address='http://www.uszip.com/zip/'+var
     conn=urllib.urlopen(address)
@@ -19,4 +14,6 @@ if __name__ == '__main__':
 	    loc=line.index('Total population')
 	    loc2=line.index('<span')
 	    print line[(loc+25):loc2]
+if __name__ == '__main__':
+    run_script()
 	    
