31f717dd8df530a022fd5d1f566a32ad54363120
Mark Giolando <mark-robin.giolando@students.olin.edu>
2013-12-12 20:49:36 -0500

    update

--- chap12/reducible.py
+++ chap12/reducible.py
@@ -1,8 +1,8 @@
 memo={}
 memo['']=['']
 def make_list():
-	"""makes word list
-	returns: list of strings"""
+	"""makes word dictionary
+	returns: dict of strings"""
 	fin=open('words.txt')
 	word_list={}
 	for line in fin:
@@ -13,15 +13,24 @@ def make_list():
 	word_list['']=''
 	return word_list
 
-def gen_children(a,word_list):
+def gen_children(word,word_list):
+	"""generates a list of child words from a given word
+	word: string
+	word_list: dict
+	"""
 	child=[]
-	for i in range(len(a)):
-		test_child=a[:(i)]+a[(i+1):]
+	for i in range(len(word)):
+		test_child=word[:(i)]+word[(i+1):]
 		if test_child in word_list:
 			child.append(test_child)
 	return child
 
 def is_reducible(word,word_list):
+	"""tests a word to see if it is reducible
+	word: string
+	word_list: dict
+	returns: bool or list
+	"""
 	if word not in word_list:
 		return False
 	if word in memo:
@@ -36,6 +45,10 @@ def is_reducible(word,word_list):
 	return res
 
 def total_reducible(word_list):
+	"""creates a list of all reducible words
+	word_list:dict
+	returns: list
+	"""
 	res=[]
 	for word in word_list:
 		temp=is_reducible(word,word_list)
@@ -43,6 +56,10 @@ def total_reducible(word_list):
 			res.append(word)
 	return res
 def longest_reducible(red_list):
+	""" finds the longest reducible word by sorting red_list by size
+	red_list: list
+	returns: list
+	"""
 	res=[]
 	for word in red_list:
 		res.append((len(word),word))
